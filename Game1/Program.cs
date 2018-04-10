f5b3df803e2b4017820240856defa534482f044d
Francis Dominic Fajardo <fajardo@duck.com>
2018-04-10 20:34:58 +0800

    Allow restarting the game through the console

--- Game1/Program.cs
+++ Game1/Program.cs
@@ -16,21 +16,44 @@ namespace Arkabound
         static void Main()
         {
             if (UseConsole)
+                writeHeader();
+
+            runGame();
+
+            if (UseConsole)
             {
-                Console.Title = "Arkabound Debug Console";
-                Console.WriteLine("/*");
-                Console.WriteLine(" * Arkabound v" + AssemblyName.GetAssemblyName(Assembly.GetExecutingAssembly().Location).Version.ToString());
-                Console.WriteLine(" * Debug Console");
-                Console.WriteLine(" */");
-                Console.WriteLine();
-                Console.WriteLine("Press any key to continue...");
-                Console.ReadKey(true);
+                while (true)
+                {
+                    Console.WriteLine("Game execution has ended, would you like to restart? Y = Yes, Other keys = No");
+                    if (Console.ReadKey(true).Key == ConsoleKey.Y)
+                    {
+                        writeHeader();
+                        runGame();
+                    }
+                    else
+                        return;
+                }
             }
+        }
+
+        static void writeHeader()
+        {
+            Console.Clear();
+            Console.Title = "Arkabound Debug Console";
+            Console.WriteLine("/*");
+            Console.WriteLine(" * Arkabound v" + AssemblyName.GetAssemblyName(Assembly.GetExecutingAssembly().Location).Version.ToString());
+            Console.WriteLine(" * Debug Console");
+            Console.WriteLine(" */");
+            Console.WriteLine();
+        }
+
+        static void runGame()
+        {
             using (var game = new MainGame())
                 game.Run();
         }
 
-        public static bool UseConsole = false;
+        public static bool UseConsole = true;
         public static bool VerboseMessages = false;
     }
 #endif
